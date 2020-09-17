1903aadcc991b773f40979719282bcba35560689
Victor Fernandes <4434814+victorfdes@users.noreply.github.com>
2020-09-18 01:31:10 +0530

    build

---
+++ docs/precache-manifest.3729b2ed21e81145ad11b519bfc9d398.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80611423648775dc00f8",
+    "url": "/css/app.fb0c6e1c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "176be04ad921eb89a2776a79ce699ee0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80611423648775dc00f8",
+    "url": "/js/app.9c17f088.js"
+  },
+  {
+    "revision": "9d752fd5dee9ef7e5092",
+    "url": "/js/chunk-vendors.7b5ac5b3.js"
+  },
+  {
+    "revision": "b0d6ded906db037aaed0ff85b3eba813",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
